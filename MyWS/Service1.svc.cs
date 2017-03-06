7b660ce895fe6488a5de451edd948fbf5854619c
IronSharq <lukitinchox@gmail.com>
2017-03-06 00:12:15 -0300

    first commit

---
+++ MyWS/Service1.svc.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace MyWS
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
+    // NOTE: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class WSPersona : IService1
+    {
+        Persona IService1.ObtenerInformacion(string Identificacion)
+        {
+            if (Identificacion == "0")
+            {
+
+                return new Persona() { nombre = "Lucas", edad = 24 };
+            }
+            else {
+
+                return new Persona() { error = "no se puede" };
+            }
+        }
+    }
+}
---
+++ TESTWS/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TESTWS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            var id = txtId.Text;
+            using (WSPersona.WSPersonaClient client = new WSPersona.WSPersonaClient())
+            {
+               var persona =  client.ObtenerInformacion(id);
+                var nombre = persona.nombre;
+                var edad = persona.edad;
+
+
+            }
+        }
+    }
+}
